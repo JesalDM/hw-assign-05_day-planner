1cfdf545a04c64d6aa596e5dd3f4f8fe0a32c095
Jesal Mehta <jesaldmehta@gmail.com>
2020-10-21 21:39:58 -0400

    Added current day in header using moment.js

--- script.js
+++ script.js
@@ -1,76 +1,55 @@
-// Dynamically create HTML elements
-    // use for-loop to create multiple rows representing each time-block
-    // Identify and add the appropriate classes from .css to elements to apply the appropriate styling
-    for (let i = 9; i< 18; i++){
-        const row = $('<div>').addClass('row time-block');
-        $('.container').append(row);
-        const timeSlot = $('<div>').addClass('hour col-1').text(timeSlotText(i));;
-        const evntText = $('<textarea>').addClass('description col-10');
-        const saveButton = $('<button>').addClass('saveBtn col-1');
-        row.append(timeSlot);
-        row.append(evntText);
-        row.append(saveButton);
-        const icon =  $('<i>').addClass('fas fa-save');
-        saveButton.append(icon);
+/*On Page load*/ 
+// Displays Current day in header using moment.js library
+$('#currentDay').text(moment().format("dddd, MMMM Do"));
+
+
+// Dynamically creates HTML elements to create multiple rows representing each time-block using for-loop
+// Appropriate classes from .css are added to elements to apply the appropriate styling
+for (let i = 9; i< 18; i++){
+    //adds an id to each row to identify each time-block
+    const row = $('<div>').addClass('row time-block').attr("id", i);;
+    $('.container').append(row);
+    //used grid system to layout the elements and make them responsive
+    // creates time-block divs and adds the text(hours)
+    const timeSlot = $('<div>').addClass('hour col-1').text(timeSlotText(i));;
+    //creates textarea for user to enter the day's work events
+    const evntText = $('<textarea>').addClass('description col-10');
+    // creates save button for every time-block for user to save the scheduled event
+    const saveButton = $('<button>').addClass('saveBtn col-1');
+    row.append(timeSlot);
+    row.append(evntText);
+    row.append(saveButton);
+    // save button icon added from font-awesome 
+    const icon =  $('<i>').addClass('fas fa-save');
+    saveButton.append(icon);
+}
+
+// this function determines the text/hour to be displayed in each time-block div
+function timeSlotText(hour){
+    let hourText = "";
+    if(hour<12){
+        hourText = hour + " AM"
+    } else if(hour>12){
+        hourText = (hour-12) + " PM" 
+    } else{
+        hourText = hour + " PM"
     }
-
-    function timeSlotText(hour){
-        let hourText = "";
-        if(hour<12){
-           hourText = hour + " AM"
-        } else if(hour>12){
-            hourText = (hour-12) + " PM" 
-        } else{
-            hourText = hour + " PM"
-        }
-        return hourText;
-    }
+    return hourText;
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Add attributes/ids to elements if needed
-
-/*On Page load*/    
-    // Get current day to display it in header
-        //use moment.js library 
-
-    // Get current time 
+// Get current time 
         //use moment.js library
 
-    // Get the entire day's schedule of events from local storage
+// Get the entire day's schedule of events from local storage
         //use localStorage.getItem()
 
-    // Display the day's schedule of events on UI. Rows color-coded based on past, present, future
+// Display the day's schedule of events on UI. Rows color-coded based on past, present, future
         // Compare each row/time-block to current time and add corresponsing CSS classes for color-coding
+
 
 /*On user action*/
     // Save the event/task when user clicks on save button after entering text into the textarea
